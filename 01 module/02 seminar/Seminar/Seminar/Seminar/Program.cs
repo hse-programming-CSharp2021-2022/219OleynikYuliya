8f831be439bd39143d3a8ce26b0ba8391b683b8e
yuoleynik <yuoleynik@edu.hse.ru>
2021-09-13 15:48:35 +0300

    HW

---
+++ 01 module/02 seminar/Seminar/Seminar/Seminar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Seminar
+{
+    class Program
+    {
+        public static int CalculateBine(uint n)
+        {
+            double b = (1 + Math.Sqrt(5)) / 2;
+            double un = (Math.Pow(b, n) - Math.Pow(-b, -n)) / (2 * b - 1);
+            return (int)(un + 0.5);
+        }
+        static void Main(string[] args)
+        {
+            uint n;
+            int res;
+            string line;
+            do
+            {
+                Console.WriteLine("Введите номер члена ряда: ");
+                line = Console.ReadLine();
+            }
+            while (!uint.TryParse(line, out n));
+            res = CalculateBine(n);
+            Console.WriteLine("Число Фибоначчи: " + res);
+            Console.WriteLine("Для выхода из программы нажмите ENTER.");
+            Console.ReadLine();
+        }
+    }
+}
