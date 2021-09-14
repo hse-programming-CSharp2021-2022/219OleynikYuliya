040cc39d46ccfbd38236ae6f042301362cb6df5e
yuoleynik <yuoleynik@edu.hse.ru>
2021-09-14 19:21:33 +0300

    SW

---
+++ 01 module/03 seminar/SW/W2/S2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace S2
+{
+    class Program
+    {
+        public static void F1()
+        {
+            int a = 1;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Math.Pow(a, 2));
+                a++;
+            }
+            Console.WriteLine();
+        }
+
+        public static void F2()
+        {
+            int b = 1;
+            while (b <= 10)
+            {
+                Console.WriteLine(Math.Pow(b, 2));
+                b++;
+            }
+            Console.WriteLine();
+        }
+
+        public static void F3()
+        {
+            int с = 1;
+            do
+            {
+                Console.WriteLine(Math.Pow(с, 2));
+                с++;
+            } while (с <= 10);
+        }
+        static void Main(string[] args)
+        {
+            F1();
+            F2();
+            F3();           
+            
+        }
+    }
+}
