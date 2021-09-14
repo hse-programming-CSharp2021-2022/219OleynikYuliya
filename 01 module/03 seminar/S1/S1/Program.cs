6e15d6df0c3fda40f813f0b2505dac0f6f3b628c
yuoleynik <yuoleynik@edu.hse.ru>
2021-09-14 19:09:42 +0300

    SW

---
+++ 01 module/03 seminar/S1/S1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Task_02
+{
+    class Program
+    {
+        public static bool Function1(bool p, bool q)
+        {
+            return !(p && q) && (!(p | !q));
+        }
+        public static void Function2(out bool x, bool p, bool q)
+        {
+            x = !(p && q) && (!(p | !q));
+        }
+        static void Main()
+        {
+            Console.WriteLine(Function1(true, true));
+            Console.WriteLine(Function1(true, false));
+            Console.WriteLine(Function1(false, true));
+            Console.WriteLine(Function1(false, false));
+            Console.WriteLine();
+            bool x;
+            Function2(out x, true, true);
+            Console.WriteLine(x);
+            Function2(out x, true, false);
+            Console.WriteLine(x);
+            Function2(out x, false, true);
+            Console.WriteLine(x);
+            Function2(out x, false, false);
+            Console.WriteLine(x);
+        }
+    }
+}
