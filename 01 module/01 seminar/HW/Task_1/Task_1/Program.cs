d5345c016cb728ca2c6195968e3c8cbd4c281418
yuoleynik <yuoleynik@edu.hse.ru>
2021-09-14 19:32:32 +0300

    HW

--- 01 module/01 seminar/HW/Task_1/Task_1/Program.cs
+++ 01 module/01 seminar/HW/Task_1/Task_1/Program.cs
@@ -4,7 +4,7 @@ namespace Task_1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
             Console.WriteLine("Hello World!");
         }
