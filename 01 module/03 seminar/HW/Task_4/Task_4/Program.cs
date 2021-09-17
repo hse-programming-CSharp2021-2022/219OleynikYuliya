9aa85af47a6de3bbb8f445f5aa474aa8d357e044
yuoleynik <yuoleynik@edu.hse.ru>
2021-09-18 00:55:42 +0300

    HW

---
+++ 01 module/03 seminar/HW/Task_4/Task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        public static void Pevertysh(int a)
+        {
+            Console.WriteLine(a % 10 * 1000 + a % 100 / 10 * 100 + a % 1000 / 100 * 10 + a / 1000);
+        }
+        static void Main(string[] args)
+        {
+            int a;
+            bool f = int.TryParse(Console.ReadLine(), out a);
+            if ((f) && (a > 999) && (a < 10000))
+            {
+                Pevertysh(a);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect input");
+            }
+        }
+    }
+}
