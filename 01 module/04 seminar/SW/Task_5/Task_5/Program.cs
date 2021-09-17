0e88aa219e7d4b56b1f5c9c0fd9866d3b10551b6
yuoleynik <yuoleynik@edu.hse.ru>
2021-09-17 14:26:07 +0300

    SW

---
+++ 01 module/04 seminar/SW/Task_5/Task_5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_5
+{
+    class Program
+    {
+        public static double Total(double k, double r, uint n)
+        {
+            if (n == 0)
+            {
+                return k;
+            }
+            return Total(k * (1 + r / 100), r, n - 1);
+        }
+        static void Main(string[] args)
+        {
+            double.TryParse(Console.ReadLine(), out double k);
+            double.TryParse(Console.ReadLine(), out double r);
+            uint.TryParse(Console.ReadLine(), out uint n);
+
+            Console.WriteLine(Total(k, r, n));
+        }
+    }
+}
