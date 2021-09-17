4c38f5ff7703d95ba40c6d0de4b00bf46e39a33a
yuoleynik <yuoleynik@edu.hse.ru>
2021-09-18 01:01:28 +0300

    SW

---
+++ 01 module/04 seminar/SW/Task_3/Task_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        public static void Recurs(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine(a);
+            }
+            else if (b > 0)
+            {
+                b /= 10;
+                a++;
+                Recurs(a, b);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int b;
+            bool f = int.TryParse(Console.ReadLine(), out b);
+            Recurs(0, b);
+        }
+    }
+}
+
