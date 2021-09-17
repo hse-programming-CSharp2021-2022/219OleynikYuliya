b35c9b54e4135928c3d4da230a828b2d7e2d4bea
yuoleynik <yuoleynik@edu.hse.ru>
2021-09-18 00:50:03 +0300

    HW

---
+++ 01 module/03 seminar/HW/Task_7/Task_7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task_7
+{
+    class Program
+    {
+        public static void First(double a)
+        {
+            Console.WriteLine((int)a);
+            Console.WriteLine(a - (int)a);
+        }
+        public static void Second(double a)
+        {
+            Console.WriteLine(a * a);
+            if (a >= 0)
+                Console.WriteLine(Math.Sqrt(a));
+            else
+                Console.WriteLine("Incorrect input"); 
+        }
+
+        static void Main(string[] args)
+        {
+            double a;
+            bool f = double.TryParse(Console.ReadLine(), out a);
+            if (f)
+            {
+                First(a);
+                Second(a);
+            }
+            else
+                Console.WriteLine("Incorrect input");
+            
+        }
+    }
+}
