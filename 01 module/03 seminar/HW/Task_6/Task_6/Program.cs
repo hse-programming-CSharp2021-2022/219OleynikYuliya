4c42cee7d4848b80b3394fcaa0ecbd70f25e0221
yuoleynik <yuoleynik@edu.hse.ru>
2021-09-18 00:34:39 +0300

    HW

---
+++ 01 module/03 seminar/HW/Task_6/Task_6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_6
+{
+    class Program
+    {
+        public static void dollary(decimal a, int b)
+        {
+            decimal c = a * b / 100;
+            Console.WriteLine("{0:C2}", c);
+        }
+        static void Main(string[] args)
+        {
+            decimal a = decimal.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            dollary(a, b);
+        }
+    }
+}
