06b285de50b5e4ef581471f1108bd9c1aa60cf02
yuoleynik <yuoleynik@edu.hse.ru>
2021-09-17 22:14:33 +0300

    HW

--- 01 module/03 seminar/HW/Task_1/Task_1/Program.cs
+++ 01 module/03 seminar/HW/Task_1/Task_1/Program.cs
@@ -4,9 +4,19 @@ namespace Task_1
 {
     class Program
     {
+        public static int Pov(int x)
+        {
+           return (x * (2 + x * ((-3) + x * (9 + x * 12))) - 4);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x;
+            bool f = int.TryParse(Console.ReadLine(), out x);
+            if (f)
+                Console.WriteLine(Pov(x));
+            else
+                Console.WriteLine("Incorrect input");
         }
     }
 }
+
