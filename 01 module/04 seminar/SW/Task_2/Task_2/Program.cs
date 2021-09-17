d2bf0232732c3be2db290c779b9a157950df053f
yuoleynik <yuoleynik@edu.hse.ru>
2021-09-17 14:18:15 +0300

    SW

--- 01 module/04 seminar/SW/Task_2/Task_2/Program.cs
+++ 01 module/04 seminar/SW/Task_2/Task_2/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace Task_2
 {
     class Program
@@ -8,24 +7,27 @@ namespace Task_2
         {
             double A;
             double delta = 0;
-            double S=0;
+            double S = 0;
             bool f1 = double.TryParse(Console.ReadLine(), out A);
             bool f2 = double.TryParse(Console.ReadLine(), out delta);
             double n = A / delta;
+            double delta2 = delta;
+            double delta3 = 0;
             if (f1 && f2)
             {
-                for (int i = 1; i <= n+1; i++)
+                for (int i = 1; i <= n + 1; i++)
                 {
-                    S += delta * (Math.Pow(delta, 2) + Math.Pow(delta + delta, 2)) / 2;
-                    delta += delta;
+                    S += delta2 * (Math.Pow(delta3, 2) + Math.Pow(delta + delta3, 2)) / 2;
+                    delta3 += delta2;
                 }
-                if (A % delta != 0)
+                if (A % delta2 != 0)
                 {
-                    double a = A - n * delta;
-                    S += a * (Math.Pow(A - a, 2) + Math.Pow(A, 2)) / 2;
+                    double a = A - n * delta2;
+                    S += delta2 * (Math.Pow(A - a, 2) + Math.Pow(A, 2)) / 2;
                 }
                 Console.WriteLine(S);
-            } else
+            }
+            else
             {
                 Console.WriteLine("Incorrect input");
             }
