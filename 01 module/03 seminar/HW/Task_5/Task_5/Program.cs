473dc9801b7d4787274cd5a088aa20347952e4e3
yuoleynik <yuoleynik@edu.hse.ru>
2021-09-18 00:24:48 +0300

    HW

---
+++ 01 module/03 seminar/HW/Task_5/Task_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_5
+{
+    class Program
+    {
+        public static void Triangle(double a, double b, double c)
+        {
+            bool f = ((a + b > c) && (a + c > b) && (b + c > a));
+            Console.WriteLine(f);
+        }
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            Triangle(a, b, c);
+        }
+    }
+}
