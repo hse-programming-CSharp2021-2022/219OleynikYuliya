f6e0c92de028dcfc1cd2ea9fd0a42882fc93e758
yuoleynik <yuoleynik@edu.hse.ru>
2021-09-28 20:46:09 +0300

    sw

---
+++ 01 module/06 seminar/SW/Task_2/Task_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Task_1
+{
+    class Program
+    {
+        static void Cout(int[] a)
+        {
+            for (int i = 0; i < a.Length; ++i)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        public static void swap(ref int a, ref int b)
+        {
+            int c;
+            c = a;
+            a = b;
+            b = c;
+        }
+        static void Main(string[] args)
+        {
+            int[] a = new int[100];
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = i+1;
+            }
+            Cout(a);
+            for (int i = 0; i < 45; i++)
+            {
+                Random rnd = new Random();
+                int i1 = rnd.Next(0, 100);
+                int i2 = rnd.Next(0, 100);
+                swap(ref a[i1], ref a[i2]);
+            }
+            Cout(a);
+            Array.Resize(ref a, a.Length-1);
+            Cout(a);
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+            Console.WriteLine(5050 - sum);
+        }
+    }
+}
