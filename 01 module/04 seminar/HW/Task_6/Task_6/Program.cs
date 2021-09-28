12abebda20fb24aaf293d25fdfd6027329a375d5
yuoleynik <yuoleynik@edu.hse.ru>
2021-09-28 16:34:32 +0300

    HW

---
+++ 01 module/04 seminar/HW/Task_6/Task_6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Task_6
+{
+    class Program
+    {
+        private static int Factorial(int a)
+        {
+            int result = 1;
+            int i = 1;
+            if (a == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                while (i <= a)
+                {
+                    result *= i;
+                    i += 1;
+                }
+            }
+            return result;
+        }
+        static double S(double x)
+        {
+            int b = 3;
+            int c = 4;
+            int d = 4;
+            double s = (1 - x * x + (Math.Pow(2, b) * Math.Pow(x, 4)) / Factorial(d));
+            do
+            {
+                b += 2;
+                c += 2;
+                d += 2;
+                s += ((Math.Pow(2, b) * Math.Pow(x, 4)) / Factorial(d));
+            }
+            while (s <= (s + ((Math.Pow(2, b) * Math.Pow(x, 4)) / Factorial(d))));
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(S(x));
+        }
+    }
+}
---
+++ 01 module/05 seminar/SW/Task_1/Task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        public static void Sums(uint number, out uint sumEven, out uint sumOdd)
+        {
+            sumEven = 0;
+            sumOdd = 0;
+            do
+            {
+                sumEven += number % 10;
+                sumOdd += number / 10 % 10;
+                number /= 100;
+            } while (number != 0);
+        }
+        static void Main(string[] args)
+        {
+            uint number = uint.Parse(Console.ReadLine());
+            uint sumEven;
+            uint sumOdd;
+            Sums(number, out sumEven, out sumOdd);
+            Console.WriteLine(sumEven);
+            Console.WriteLine(sumOdd);
+        }
+    }
+}
