94ffd4f1e2c543fbf69b561d90a585f226d9a939
yuoleynik <svvokey@mail.ru>
2021-10-01 15:27:45 +0300

    sw

--- 01 module/08 seminar/SW/Task_1/Program.cs
+++ 01 module/08 seminar/SW/Task_1/Program.cs
@@ -4,13 +4,6 @@ namespace Task_1
 {
     class Program
     {
-        public static int Comp(int i, int i1)
-        {
-            if (i % 2 == 0 && i1 % 2 != 0)
-                return 1;
-            return -1;
-        }
-
         public static int Sum(int a)
         {
             int sum = 0;
@@ -21,9 +14,33 @@ namespace Task_1
             }
             return sum;
         }
+        public static int Sum2(int a)
+        {
+            int sum = 0;
+            while (a > 0)
+            {
+                sum += a % 10;
+                a /= 10;
+            }
+            return sum;
+        }
+        public static int Comp(int i, int i1)
+        {
+            if (i % 2 == 0 && i1 % 2 != 0)
+                return 1;
+            return -1;
+        }
+
         public static int Comp1(int i, int i1)
         {
             if (Sum(i) > Sum(i1))
+                return 1;
+            return -1;
+        }
+
+        public static int Comp2(int i, int i1)
+        {
+            if (Sum2(i) > Sum2(i1))
                 return 1;
             return -1;
         }
@@ -43,8 +60,9 @@ namespace Task_1
             Array.Sort(a, Comp1);
             Array.ForEach(a, el => Console.Write(el + " "));
             Console.WriteLine();
+            Array.Sort(a, Comp2);
+            Array.ForEach(a, el => Console.Write(el + " "));
             Console.WriteLine();
-
         }
     }
 }
