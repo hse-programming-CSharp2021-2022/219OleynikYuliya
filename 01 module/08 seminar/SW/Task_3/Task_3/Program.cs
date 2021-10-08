4239347f630124f104351d401cfc985501510e19
yuoleynik <yuoleynik@edu.hse.ru>
2021-10-08 10:12:16 +0300

    SW

---
+++ 01 module/08 seminar/SW/Task_3/Task_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(1000, 10000);
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.WriteLine();
+            int[] p = Array.FindAll(arr, Flag);
+            for (int i = 0; i < p.Length; i++)
+            {
+                Console.WriteLine(p[i]);
+            }
+        }
+
+        static bool Flag(int a)
+        {
+            int[] arr = new int[4];
+            arr[0] = a % 10;
+            arr[1] = (a / 10) % 10;
+            arr[2] = (a / 100) % 10;
+            arr[3] = (a / 1000) % 10;
+            if (arr[0] == arr[3] && (arr[1] == arr[2]))
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
