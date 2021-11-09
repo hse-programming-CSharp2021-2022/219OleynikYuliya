6a34b6c28e97f46a721630aef9f791480e642596
yuoleynik <yuoleynik@edu.hse.ru>
2021-11-09 16:28:21 +0300

    SW

---
+++ 02 module/01 seminar/SW/Task_1/Task_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task_2
+{
+    class RegularPolygon
+    {
+        public int N { get; set; }
+        public double R { get; set; }
+
+        public RegularPolygon(int n = 0, double r = 0)
+        {
+            N = n;
+            R = r;
+        }
+        public double S
+        {
+            get
+            {
+                return N * 2 * R * Math.Tan(Math.PI / N);
+            }
+        }
+
+        public double P
+        {
+            get
+            {
+                return R * P/2;
+            }
+        }
+        public string PolygonData()
+        {
+            return N + " " + R + " " + P + " " + S;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegularPolygon polygon = new RegularPolygon(10, 10);
+            Console.WriteLine(polygon.PolygonData());
+        }
+    }
+}
