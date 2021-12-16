c926fe9ede2ceac31bfaf2b14489e970d3be66b9
Юлия Олейник <yuoleynik@edu.hse.ru>
2021-12-17 02:57:22 +0600

    HW

---
+++ 02 module/11 seminar/SW/Task_2/Task_2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        int i = 0;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text += sender.ToString()[sender.ToString().Length - 1];
+            i+=1;
+        }
+
+        private void delete_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = textBlock.Text[..^1];
+        }
+    }
+}
